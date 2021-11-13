aecc81ebbbdb66d271eb341582c149d59a9e1cd4
widwiew <wiwis.jaitab@gmail.com>
2021-11-13 08:47:25 +0700

    add test

---
+++ backend/apis/version1/route_jobs.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+
+from db.session import get_db
+from db.models.jobs import Job
+from schemas.jobs import JobCreate, ShowJob
+from db.repository.jobs import create_new_job, retreive_job
+from schemas.jobs import ShowJob
+
+router = APIRouter()
+
+@router.post("/create-job", response_model=ShowJob)
+def create_job(job: JobCreate, db: Session = Depends(get_db)):
+    owner_id = 1
+    job = create_new_job(job=job, db=db, owner_id=owner_id)
+    return job
+
+@router.get("/get/{id}", response_model=ShowJob)
+def retreive_job_by_id(id:int, db:Session = Depends(get_db)):
+    job = retreive_job(id=id, db=db)
+    print(job)
+    if not job:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND
+        , detail=f"Job with id {id} does not exist")
+    return job
--- backend/db/session.py
+++ backend/db/session.py
@@ -7,6 +7,9 @@ from typing import Generator
 SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db() -> Generator:
