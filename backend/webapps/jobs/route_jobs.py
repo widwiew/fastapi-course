f6f64ea8b90d4175f6cf355466e07f8ae30d0695
widwiew <wiwis.jaitab@gmail.com>
2021-11-29 16:21:58 +0700

    Add frontend

---
+++ backend/webapps/jobs/route_jobs.py
@@ -0,0 +1,23 @@
+from sqlalchemy.orm.session import Session
+from fastapi import APIRouter
+from fastapi import Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from db.repository.jobs import list_jobs, retreive_job
+from sqlalchemy.orm import Session
+from db.session import get_db
+from fastapi import Depends
+
+templates = Jinja2Templates(directory="templates")
+router = APIRouter(include_in_schema=False)
+
+@router.get("/")
+def home(request:Request, db:Session = Depends(get_db), msg:str=None):
+    # print(dir(request))
+    jobs = list_jobs(db=db)
+    return templates.TemplateResponse("jobs/homepage.html", {"request":request, "jobs":jobs, "msg":msg})
+
+@router.get("/detail/{id}")
+def job_detail(id:int, request:Request, db:Session = Depends(get_db)):
+    job = retreive_job(id=id, db=db)
+    return templates.TemplateResponse("jobs/detail.html", {"request":request, "job":job})
